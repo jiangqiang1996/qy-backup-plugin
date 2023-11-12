b54bdf7b7ffc06c9dc0874434d65b9efc5c4f1d1
jiangqiang <jq1257403419@vip.qq.com>
2023-11-12 20:26:44 +0800

    fix

--- src/main/java/run/halo/starter/StarterPlugin.java
+++ src/main/java/run/halo/starter/StarterPlugin.java
@@ -5,38 +5,14 @@ import cn.hutool.core.date.TemporalAccessorUtil;
 import cn.hutool.core.thread.ThreadUtil;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
-import cn.hutool.extra.spring.SpringUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.pf4j.ExtensionFinder;
-import org.pf4j.ExtensionWrapper;
-import org.pf4j.PluginRuntimeException;
 import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferWrapper;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.lang.NonNull;
-import org.springframework.lang.Nullable;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.server.ServerErrorException;
-import org.springframework.web.server.ServerWebInputException;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import run.halo.app.core.extension.attachment.Attachment;
-import run.halo.app.core.extension.attachment.Policy;
-import run.halo.app.core.extension.attachment.endpoint.AttachmentHandler;
-import run.halo.app.core.extension.attachment.endpoint.UploadOption;
 import run.halo.app.core.extension.service.AttachmentService;
-import run.halo.app.extension.ConfigMap;
 import run.halo.app.extension.ExtensionClient;
 import run.halo.app.extension.Metadata;
 import run.halo.app.extension.ReactiveExtensionClient;
@@ -101,10 +77,11 @@ public class StarterPlugin extends BasePlugin {
         backup.setSpec(spec);
         extensionClient.create(backup);
 
-
         ThreadUtil.schedule(scheduledThreadPoolExecutor, () -> {
             System.out.println("定时任务");
-            upload("default-policy", "", "test", Flux.fromArray(new DataBuffer[0]), MediaType.ALL);
+            System.out.println(this.attachmentService);
+            this.attachmentService.upload("default-policy", "", "test",
+                Flux.fromArray(new DataBuffer[0]), MediaType.ALL);
         }, 0, 1, TimeUnit.SECONDS, true);
         System.out.println("插件启动成功start！");
     }
@@ -123,56 +100,4 @@ public class StarterPlugin extends BasePlugin {
         System.out.println("插件删除delete！");
     }
 
-    public Mono<Attachment> upload(@NonNull String policyName,
-        @Nullable String groupName,
-        @NonNull String filename,
-        @NonNull Flux<DataBuffer> content,
-        @Nullable MediaType mediaType) {
-        return authenticationConsumer(
-            authentication -> reactiveExtensionClient.get(Policy.class, policyName)
-                .flatMap(policy -> {
-                    var configMapName = policy.getSpec().getConfigMapName();
-                    if (!StringUtils.hasText(configMapName)) {
-                        return Mono.error(new ServerWebInputException(
-                            "ConfigMap name not found in Policy " + policyName));
-                    }
-                    return reactiveExtensionClient.get(ConfigMap.class, configMapName)
-                        .map(configMap -> UploadOption.from(filename,
-                            content,
-                            mediaType,
-                            policy,
-                            configMap));
-                })
-                .flatMap(uploadContext -> {
-                    List<AttachmentHandler> components = new ArrayList<>();
-                    components.addAll(SpringUtil.getBeansOfType(AttachmentHandler.class).values());
-                    var handlers = List.copyOf(components);
-                    return Flux.fromIterable(handlers)
-                        .concatMap(handler -> handler.upload(uploadContext))
-                        .next();
-                })
-                .switchIfEmpty(Mono.error(() -> new ServerErrorException(
-                    "No suitable handler found for uploading the attachment.", null)))
-                .doOnNext(attachment -> {
-                    var spec = attachment.getSpec();
-                    if (spec == null) {
-                        spec = new Attachment.AttachmentSpec();
-                        attachment.setSpec(spec);
-                    }
-                    spec.setOwnerName(authentication.getName());
-                    if (StringUtils.hasText(groupName)) {
-                        spec.setGroupName(groupName);
-                    }
-                    spec.setPolicyName(policyName);
-                })
-                .flatMap(reactiveExtensionClient::create));
-    }
-
-    private <T> Mono<T> authenticationConsumer(Function<Authentication, Mono<T>> func) {
-        return ReactiveSecurityContextHolder.getContext()
-            .switchIfEmpty(Mono.error(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED,
-                "Authentication required.")))
-            .map(SecurityContext::getAuthentication)
-            .flatMap(func);
-    }
 }
